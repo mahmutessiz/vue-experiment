3a2dab1e65a38defea741c205a796a2d91fa6eaa
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-01-24 18:27:39 +0300

    .

--- router-exp/router-deneme/src/components/HomeView.vue
+++ router-exp/router-deneme/src/components/HomeView.vue
@@ -5,13 +5,16 @@ export default {
     data() {
         return {
             onOf: false,
+            deneme: {
+                dene: '<img style="width: 50%;"  src="https://wallpapers.com/images/hd/dark-anime-gothic-girl-k1uj3cr7kr9a96nx.jpg" alt="">',
+            }
         }
     },
     methods: {
         confeti() {
             const jsConfetti = new JSConfetti()
             jsConfetti.addConfetti({
-                emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+                emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸',"😎"],
             });
         }
     },
@@ -20,9 +23,21 @@ export default {
 </script>
 
 <template>
+
+    <div style="width: 100%; display: grid; justify-content: center; align-items: center;">
+    <button @click="confeti" style="width: 35%;">confeti</button>
+
+        <div>
+            {{ deneme.dene }}
+        </div>
+        <br>
+
+        <div style="width: 100%;" v-html="deneme.dene"></div>
+    </div>
     <br><br><br>
     <h1> ana sayfa</h1>
 
-    <button @click="confeti">confeti</button>
+
+
 
 </template>
