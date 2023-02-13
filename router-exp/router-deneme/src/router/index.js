7a1f2d142b5d270b77661874295849924c988804
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-02-13 12:16:27 +0300

    .

--- router-exp/router-deneme/src/router/index.js
+++ router-exp/router-deneme/src/router/index.js
@@ -4,8 +4,6 @@ import AboutView from "../components/AboutView.vue";
 import HomeView from "../components/HomeView.vue";
 
 const router= createRouter({
-    mode: "history",
-    base: process.env.BASE_URL,
     history : createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
